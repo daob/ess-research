d64a74e1a6d3178f44ab70798762e91dfd3a0a3a
daniel.oberski <daniel.oberski@a4da8fd2-e337-0410-bd9a-b1cab423ab52>
2007-08-30 13:39:17 +0000

    Modified women smallmodel, now fits introduced more constraints for overparameterized Greece, corrected two restrictions based on highest MI rather than random threshold, freed one parameter in Slovenia.

--- trunk/input/plot-power.R
+++ trunk/input/plot-power.R
@@ -7,8 +7,8 @@ highpower <- .80 # What constitutes "high power"?
 
 critical  <- qchisq( 1-alpha, 1 ) # Critical value of chisquare with 1 df
 
- mi.dat <- read.table("~/current-project/output/women-MI", header=T)
- mi.dat$ncp <-  ( mi.dat$mi / mi.dat$epc^2 ) * effsize^2
+ mi.dat <- read.table("C:\\ESS\\berlin\\output\\women-MI", header=T)
+ mi.dat$ncp <-  ( mi.dat$mi / mi.dat$stdXYEPC^2 ) * effsize^2
  mi.dat$CNTRY <- c(rep(1,24),rep(2,24),rep(3,24),rep(4,24))
  mi.dat$power <- round( 1 - pchisq( critical, 1, ncp=mi.dat$ncp), 2 )
 
@@ -20,20 +20,20 @@ critical  <- qchisq( 1-alpha, 1 ) # Critical value of chisquare with 1 df
 # pdf("~/current-project/output/pics/power.pdf")
 
  #initially plot only the lower-than-effect size points as circles:
- plot( power[epc < effsize],
-      mi[epc < effsize],
+ plot( power[stdXYEPC < effsize],
+      mi[stdXYEPC < effsize],
       xlab="Power", ylab="Modification Index (MI)")
  abline( critical, 0 ) # line indicating critical value of the Chi-square
  lines( c( highpower, highpower ), c(0,80) ) # line indicating "high power"
- # triangles indicate EPC >= the searched-for effect size:
- points(power[epc >= effsize], mi[epc >= effsize], pch=24 )
+ # triangles indicate STDXYEPC >= the searched-for effect size:
+ points(power[stdXYEPC >= effsize], mi[stdXYEPC >= effsize], pch=24 )
 
 # dev.off()
 
 
 identify(power,mi, labels=paste(param,CNTRY), plot=T )
 
-misspecified <- ( mi >= critical & power < highpower) | (mi >= critical & power >= highpower & epc >= effsize )
+misspecified <- ( mi >= critical & power < highpower) | (mi >= critical & power >= highpower & stdXYEPC >= effsize )
 table(misspecified, mi.dat$CNTRY)
 paste( mi.dat$param[misspecified], mi.dat$CNTRY[misspecified] )
 
