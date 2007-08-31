c1cf199f1550edefe1f7748fe0350c80512e29b4
daniel.oberski <daniel.oberski@a4da8fd2-e337-0410-bd9a-b1cab423ab52>
2007-08-31 16:41:37 +0000

    added EQS input/eqs added estimated covariance matrices and relative means comparing coefficients continuous-categorical analyses    high score for CZ 1,1 =??

---
+++ trunk/input/read-moments.R
@@ -0,0 +1,30 @@
+
+rm(list=ls())
+
+dat <- read.table("C:\\ESS\\berlin\\output\\women-smallmodel-covariances-R", header=F)
+dat <- dat[,1:2]
+
+outpath <- "C:\\ESS\\berlin\\data\\"
+
+countries <- c('CH','CZ','GR','SI')
+numvars <- 6
+numpercountry <- ( numvars*(numvars+1)/2 ) + numvars
+
+covars <- list()
+means <- list()
+for(cntry in 1:length(countries)) {
+  covars[[cntry]] <- matrix( rep(NA,numvars^2), numvars, numvars )
+  diag( covars[[cntry]] ) <- dat[1:numvars,2]
+  start <- 7
+  i <- 1
+
+  for(i in 1:(numvars-1)) {
+    covars[[cntry]][(i+1):numvars,i]<- dat[ start:(start+(numvars-i-1)), 2 ]
+    start <- start+numvars-i
+  }
+  means[[cntry]] <- dat[ start:(start+(numvars-1)), 2]
+  write.table(covars[[cntry]], paste(outpath,'women_',countries[cntry],'.cov',sep=""),row.names=F,col.names=F,na="" )
+    write.table(means[[cntry]], paste(outpath,'women_',countries[cntry],'.mean',sep=""),row.names=F,col.names=F,na="" )
+  start <- start+(numvars-1)
+  dat <- dat[(numpercountry+1):NROW(dat),]
+}
