a67f8f5c8a2b7bc1d499801b6fdaccf2e3135d4e
daniel.oberski <daniel.oberski@a4da8fd2-e337-0410-bd9a-b1cab423ab52>
2007-11-18 14:25:23 +0000

    string wrapping, all experiments

--- trunk/input/drawthresholds.R
+++ trunk/input/drawthresholds.R
@@ -1,4 +1,8 @@
-experiments <- "women"
+require(QCA) # for prettyString
+
+rm(list=ls())
+
+experiments <- "job"
 
 thresholds <- read.table(paste("~/current-project/data/",experiments,"/thresholds.txt",sep=""), header=T)
 means <- read.table(paste("~/current-project/data/",experiments,"/means.txt",sep=""), header=T)
@@ -31,6 +35,7 @@ for (ie in 1:numexperiments) {
                 tau <- thresholds[thresholds$country==countries[ic] &  thresholds$trait==traits[it] &  thresholds$method==methods[im] ,1]
                 nu <- means[means$country==countries[ic] &  means$trait==traits[it] &  means$method==methods[im], 1]
                 tau <- tau - nu
+                numtau <- length(tau)
                 tau.mat <- rbind(tau.mat, c(tau,countries[ic],traits[it],methods[im]))
             }  
         }
@@ -38,7 +43,11 @@ for (ie in 1:numexperiments) {
 }
 
 
+pdf(paste("~/current-project/output/thresholds_",experiments,".pdf",sep=""),
+    height=nummethods*2.5, width=numtraits*4.2 )
+
 par(mfcol=c(nummethods,numtraits))
+par(mar=c(6,2,2,2))
 zlim <- c(-3,3) #min and max z-scores to draw. default=3 sd's
 
 for(it in 1:numtraits) {
@@ -48,16 +57,18 @@ for(it in 1:numtraits) {
     print(numtraits*(im-1)+it)    
     
     ref.y <- 0:(numcountries-1) #  y posititions at which to draw the reference lines
+    string.vector <- unlist(strsplit(questions[numtraits*(im-1)+it,1], " "))
+    title.text <- prettyString(string.vector, 60, 0, " ")
     
     plot(0,0, xlim=zlim,
          ylim=c(-.1, ref.y[length(ref.y)]+.30),
-         main=questions[numtraits*(im-1)+it,1],
+         main=title.text,
          xlab="z-score",
          ylab="Country",
          yaxt='n', pch=NA,
          frame.plot=F,
          font.main=3,
-         cex.main=.8
+         cex.main=1,
         )
     
     axis(2, ref.y,labels=as.character(countries))
@@ -65,7 +76,6 @@ for(it in 1:numtraits) {
     #draw reference lines
     lapply(ref.y, function(x){ lines(zlim, c(x,x)) } )
 
-    numtau <- 4
     tau.cur <- tau.mat[
                        tau.mat[,numtau+2]==it & tau.mat[,numtau+3] == im,
                        1:numtau
@@ -87,6 +97,7 @@ for(it in 1:numtraits) {
   }
 }                                        
 
+dev.off()
 
 # men gebruikt dat deel van de schaal waar men het mee eens is
 # 'negatieve' vraagstelling beinvloedt de overgangen
