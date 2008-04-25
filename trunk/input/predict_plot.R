2eb5c999a60026e4f2306d2f81c97e4d573adcdb
daniel.oberski <daniel.oberski@a4da8fd2-e337-0410-bd9a-b1cab423ab52>
2008-04-25 09:08:08 +0000

    oops double paragraphs

--- trunk/input/predict_plot.R
+++ trunk/input/predict_plot.R
@@ -2,51 +2,52 @@
 #exp( log(c)*b + a )
 
 meta <- read.table("~/current-project/data/meta.tab", header=T)
-meta <- meta[,]             # tried deleting points 37, 43, 49 in sensitivity analysis.
+meta$ptnames <- 1:nrow(meta)
+meta <- meta[ -c( 28, 2, 11, 49 ), ]             # tried deleting points 28, 2, 11, 49
 
+
+# Estimate a log-log linear model for each topic separately
 list.models <- by(cbind(log(meta$q2),log(meta$cat_factor)), meta$topic, lm )
 
- predict.exp.women <- function(c) {
-    b <-  list.models$women$coef[2]
-    a <-  list.models$women$coef[1]
-    exp( log(c)*b + a )  
- }
+# The names of the experiments as they are to appear in the graph
+experiment.name <- c(
+                     doctors  = "Social distance",
+                     efficacy = "Efficacy",
+                     job      = "Job",
+                     women    = "Role of women"
+                    )
+
+predict.exp <- function(c, subgroup) {
+  # provides a function to plot for a given topic
+  b <-  list.models[[subgroup]]$coef[2]
+  a <-  list.models[[subgroup]]$coef[1]
+  exp( log(c)*b + a )  
+}
  
-  predict.exp.doctors <- function(c) {
-    b <-  list.models$doctors$coef[2]
-    a <-  list.models$doctors$coef[1]
-    exp( log(c)*b + a )  
- }
- 
-  predict.exp.job <- function(c) {
-    b <-  list.models$job$coef[2]
-    a <-  list.models$job$coef[1]
-    exp( log(c)*b + a )  
- }
- 
-  predict.exp.efficacy <- function(c) {
-    b <-  list.models$efficacy$coef[2]
-    a <-  list.models$efficacy$coef[1]
-    exp( log(c)*b + a )  
- }
- 
+plot.subset <- function(subgroup) {
+  # produces 1 plot of the observed data and predicted curve for a given topic
+  tmp <- subset(meta, topic==subgroup)
+  predict.local <- function(x) {predict.exp(x, subgroup)}
+  curve(predict.local, 0.40, 2, ylim=range(meta$q2), main=experiment.name[subgroup], xlab=xlab, ylab=ylab)
+# Uncomment this to get the point ID number in the graph instead of a black dot:  
+#  text(tmp$cat_factor, tmp$q2, labels=as.character(tmp$ptnames) )
+  points(tmp$cat_factor, tmp$q2, pch=20)  
+
+}
+
+plot.meta <- function(meta)  {
+  # produces one graph with each of the plots corresponding to a model
+  par(mfrow=c(2,2))
+  par(oma=c(0,0,0,0))
+  par(mar=c(3,3,3,1.5), mgp=c(2,1,0))
+
+  xlab <- "Categorisation factor"
+  ylab <- "Quality (continuous model)"
+  
+  lapply(levels(meta$topic), plot.subset)
+}
+
+
 pdf("~/current-project/latex/i/predict_q2.pdf")
- 
-opar<-par()
-par(mfrow=c(2,2))
-par(oma=c(0,0,0,0))
-par(mar=c(3,3,3,1.5), mgp=c(2,1,0))
-
-curve(predict.exp.doctors, 0.40, 2, ylim=c(min(q2),max(q2)), main="Social distance", xlab="Categorisation factor", ylab="Quality (continuous model)")
-points(cat_factor[topic=='doctors'], q2[topic=='doctors'], pch=20)
-
-curve(predict.exp.efficacy, 0.40, 2, ylim=c(min(q2),max(q2)),  main="Efficacy", xlab="Categorisation factor", ylab="Quality (continuous model)")
-points(cat_factor[topic=='efficacy'], q2[topic=='efficacy'], pch=20)
-
-curve(predict.exp.job, 0.40, 2, ylim=c(min(q2),max(q2)),  main="Job", xlab="Categorisation factor", ylab="Quality (continuous model)")
-points(cat_factor[topic=='job'], q2[topic=='job'], pch=20)
-
-curve(predict.exp.women, 0.40, 2, ylim=c(min(q2),max(q2)), main="Role of women", xlab="Categorisation factor", ylab="Quality (continuous model)")
-points(cat_factor[topic=='women'], q2[topic=='women'], pch=20)
-
+plot.meta(meta)
 dev.off()
